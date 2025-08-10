3e7fb24930407f23c33d3c0b596058b090b0ecf2
HB <matosh38@gmail.com>
2025-08-10 16:58:37 -0300

    Criando função que adiciona os dados do funcionario em uma lista e retorna essa lista

--- ReadPDF.py
+++ ReadPDF.py
@@ -2,19 +2,23 @@ import pdfplumber as pp
 
 path = "ficha.pdf"
 
+data = ["Empregado ", "Data de nascimento", " ",]
 # Verifica se o nome está na lista, retorna o indíce da linha
 def find_line(name):
     for i, k in enumerate(lines):
-        print(i, k)
-        if name in lines:
-            return i
+        if name in k:
+            return lines[i+1]
+
+def EmpData(lista):
+    for i, k in enumerate(data):    
+        if k is not " ":
+            lista.append(find_line(k))
+        lista.append("M")
+    return lista
 
 # Abre o arquivo pdf e transforma todo o pdf em texto
 with pp.open(path) as pdf:
     txt = ""
     for page in pdf.pages:
         txt += page.extract_text()+'\n'
-
 lines = txt.splitlines()
-#line_number = find_line(keyWord)
-print(lines[18])

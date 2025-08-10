bc3f580f9a393ab12f633edf8326c8242aff628c
HB <matosh38@gmail.com>
2025-08-10 17:02:14 -0300

    Modificando a planilha

--- Dados_funcionario.py
+++ Dados_funcionario.py
@@ -1,15 +1,35 @@
 from openpyxl import Workbook, load_workbook
+from ReadPDF import *
 import os
+
+data = ["Nome", "Nascimento", "Sexo","CEP", "Bairro","Tipo lougradouro", "Lougradouro", "Número", "Complemento", "Cidade", "UF", "CPF", "RG", "Data de Emissão", "Orgão emissor", "UF"]
+employer_data = []
+
 
 # O nome por enquanto será pré-definido depois o usúario será responsavel por passar um arquivo já existente ou criar um novo
 path_file = "Controle_Funcionario.xlsx"
 
 try:
     wb = load_workbook(path_file)
+    for i, row in enumerate(wb.active.iter_rows(values_only=True), start=1):
+        if any(cell is not None for cell in row):
+            line = i+1
 except FileNotFoundError:
     wb = Workbook()    
+    line = 2
+    
 ws = wb.active
 
-# Area do preenchimento da planilha
+for i, k in enumerate(data):
+    ws.cell(row=1, column=i+1, value=data[i])
+
+linha = find_line("Empregado ")
+
+data = find_line("Data de nascimento").split(" ")
+nascimento = data[0]
+
+
+# Area do preenchimento da planilha 
 
 wb.save(path_file)
+os.system(f"start {path_file}")
